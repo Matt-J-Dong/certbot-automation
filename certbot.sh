613a17abdaf1ac753b3792786a3722f8ec4bd973
BloodhunterD <bloodhunterd@bytearts.net>
2019-08-20 10:33:56 +0200

    Domain loading adjusted for new list format. Use default certificate keysize, if not set in configuration. Reload NGINX only, if container name is set in configuration.

--- certbot.sh
+++ certbot.sh
@@ -3,29 +3,38 @@
 # Include config
 . ./certbot.conf
 
+# Load domains
+readarray -t DOMAINS < ./certbot.domains
+
 LOG_FILE="${CERTBOT_PATH}${LOG_FILE}"
 
-# Include domains from external file
-. $CERTBOT_PATH/domains.list
+# Use default keysize, if not set
+if [ -z ${KEYSIZE+x} ]; then
+	KEYSIZE=4096
+fi
 
-for ((i=0;i<${#DOMAIN[*]};i++))
+# Check all domains for renewal
+for ((i=0;i<${#DOMAINS[*]};i++))
 do
-	# Certbox requests a new certificate, if the existing expires in less than two weeks
-	docker run --rm -it --name certbot -v "${CERTBOT_PATH}/letsencrypt/:/etc/letsencrypt/" -v "${CERTBOT_PATH}/log/:/var/log/letsencrypt/" -v "${CERTBOT_PATH}/well-known/:/var/www/letsencrypt/" certbot/certbot certonly --agree-tos --email ${EMAIL} --keep --quiet --rsa-key-size ${KEYSIZE} --webroot -w /var/www/letsencrypt --cert-name ${DOMAIN[i]} -d ${DOMAIN[i]}
+	# Certbot requests a new certificate, if the existing expires in less than two weeks
+	docker run --rm -it --name certbot -v "${CERTBOT_PATH}/letsencrypt/:/etc/letsencrypt/" -v "${CERTBOT_PATH}/log/:/var/log/letsencrypt/" -v "${CERTBOT_PATH}/well-known/:/var/www/letsencrypt/" certbot/certbot certonly --agree-tos --email ${EMAIL} --keep --quiet --rsa-key-size ${KEYSIZE} --webroot -w /var/www/letsencrypt --cert-name ${DOMAINS[i]} -d ${DOMAINS[i]}
 done
 
-for ((i=0;i<${#DOMAIN[*]};i++))
-do
-	# A new certificate was created
-	if [ `find "${CERTBOT_PATH}/letsencrypt/live/${DOMAIN[i]}/cert.pem" -mmin -5` ]; then
-		CHANGED=true
+# Reload NGINX Docker container, if set and a certificate was renewed
+if [ -z ${NGINX_CONTAINER_NAME+x} ]; then
+	for ((i=0;i<${#DOMAINS[*]};i++))
+	do
+		# A certificate was renewed
+		if [ `find "${CERTBOT_PATH}/letsencrypt/live/${DOMAINS[i]}/cert.pem" -mmin -10` ]; then
+			CHANGED=true
+		fi
+	done
+
+	# Reload NGINX and check, if it's running after
+	if [ "${CHANGED}" = true ]; then
+		DATE=$(date +"%Y-%m-%d %H:%M:%S")
+		RELOAD=$(docker exec ${NGINX_CONTAINER_NAME} bash service nginx reload)
+		RESTART=$(docker exec ${NGINX_CONTAINER_NAME} bash service nginx status)
+		echo "${DATE} | ${RELOAD} ${RESTART}" >> $LOG_FILE
 	fi
-done
-
-# Reload NGINX and check, if it's running after
-if [ "${CHANGED}" = true ]; then
-	DATE=$(date +"%Y-%m-%d %H:%M:%S")
-	RELOAD=$(docker exec ${NGINX_CONTAINER_NAME} bash service nginx reload)
-	RESTART=$(docker exec ${NGINX_CONTAINER_NAME} bash service nginx status)
-	echo "${DATE} | ${RELOAD} ${RESTART}" >> $LOG_FILE
 fi
